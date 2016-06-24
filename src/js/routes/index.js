63c32df000ebe03e924c3578611841b967a28554
Rauliyohmc <rauliyohmc@gmail.com>
2016-06-24 17:54:03 +0200

    Providing explanation of the auth method used via comments

--- src/js/routes/index.js
+++ src/js/routes/index.js
@@ -3,6 +3,16 @@ import { Route } from 'react-router'
 import App from '../components/App'
 import { authURL } from '../constants'
 
+/**
+ * Hook to manage Instagram authentication and authorization
+ * @see https://www.instagram.com/developer/authentication/
+ * For simplicity (although less secure), we are leveraging Client-Side (Implicit) Authentication
+ * Once we login and authorize through Instagram, it will append the access_token in the URL fragment as part of the hash
+ * in our case, http://localhost:3000/#access_token=ACCESS-TOKEN
+ *
+ * @param nextState
+ * @param replace
+ */
 function checkAccessToken(nextState, replace) {
   // We need an access token to start using the instagram API!
   const { hash } = nextState.location;
@@ -19,6 +29,6 @@ function checkAccessToken(nextState, replace) {
 
 export default (
   <Route path="/" component={App} onEnter={checkAccessToken} >
-    
+
   </Route>
 )
